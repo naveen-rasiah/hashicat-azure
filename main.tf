e61468469f95b7d821c2f6dc3ec4e952e85d45fe
Naveen Rasiah <naveen.rasiah@gmail.com>
2019-12-09 15:18:57 -0500

    testing

--- main.tf
+++ main.tf
@@ -2,4 +2,30 @@ resource "azurerm_resource_group" "myresourcegroup" {
   name     = "${var.prefix}-workshop"
   location = "${var.location}"
 }
-#
+
+
+module "web_app_container" {
+
+source = "app.terraform.io/Naveen-Cool-Org/web-app-container/azurerm"
+
+version = "2.2.1"
+
+name = "${var.prefix}-app"
+
+port = "80"
+
+https_only = "false"
+
+resource_group_name = "${azurerm_resource_group.myresourcegroup.name}"
+
+container_type = "docker"
+
+container_image = "scarolan/palacearcade"
+
+}
+
+output "container_app_url" {
+
+value = "http://${module.web_app_container.hostname}"
+
+}
